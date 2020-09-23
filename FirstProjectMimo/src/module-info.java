c7d86a8340ed0368972ab5c21544314459504d63
Emily <Emily@viva-la-vida.lan>
2020-09-23 02:33:43 -0500

    initial commit

---
+++ FirstProjectMimo/src/module-info.java
@@ -0,0 +1,14 @@
+public class SpaceAdventure {
+	public static void main
+	(String[] args) {
+		String s1 = "Colonel";
+		String s2 = "Samantha";
+		String captain = s1+" "+s2;
+		System.out.print(captain);
+		
+		int planetCount=0;
+		planetCount++;
+		PlanetCount +=2;
+		System.out.print ("Sam has seen" + planetCount + " planets");
+	}
+}
