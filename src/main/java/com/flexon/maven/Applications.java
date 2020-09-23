8a3ac33eda9cec85ef8504638761bccfaff8e698
Jiarui Li <Emily@Viva-la-vida.lan>
2020-09-22 21:59:23 -0500

    First commit

---
+++ src/main/java/com/flexon/maven/Applications.java
@@ -0,0 +1,21 @@
+package com.flexon.maven;
+
+public class Applications {
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+    
+    public static int sum(int num1, int num2){
+		int results;
+		results = num1+num2;
+    		return results;
+    }
+    
+    public static int difference(int num1, int num2){
+		int results;
+		results = num1-num2;
+    		return results;
+    }
+}
---
+++ src/test/java/com/flexon/maven/AppTest.java
@@ -0,0 +1,58 @@
+package com.flexon.maven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        System.out.println("This is Test Case 1");
+    }
+	
+	@Test
+	public void testSumMethod(){
+		int result;
+		result = Applications.sum(1, 1);
+		
+    	    assertEquals(2, result);
+    	    System.out.println("Test sum 1 + 1 passed!!");
+	}
+	
+	@Test
+	public void testSumMethod1(){
+		int result;
+		result = Applications.sum(2, -1);
+		
+    	    assertEquals(1, result);
+    	    System.out.println("Test sum 2 + -1 passed!!");
+
+	}
+	
+    
+	/*
+    @Test
+    public void testSumPositive() {
+    	int results;
+    	
+    	System.out.println("Sum Test case 1");
+    	results = Applications.sum(0, 0);
+    	
+    	assertEquals(0, results);
+    	
+    }
+    
+    @Test
+    public void testcase2() {
+    	int results;
+    	
+    	System.out.println("Sum Test case 2");
+    	results = Applications.sum(1, 2);
+    	
+    	assertEquals(3, results);
+    }
+    */
+	
+}
